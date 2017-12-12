81dc13fa388157b4fa3f5818706f600a27ecb833
Joshua Bartlett <jcbart12595@gmail.com>
2017-12-11 21:10:17 -0500

    putting the actual trends back on the homepage

--- js/searchhomepage.js
+++ js/searchhomepage.js
@@ -14,3 +14,76 @@ $(function () {
     });
 	
 });
+
+var cb = new Codebird;
+cb.setConsumerKey("s75bp2rPR15pzp8wQFIuul24U", "aYaHZQBCvDXD3kx9svd5WGLKLUuRPqyvuKckGY0iAZ9SgnYSFF");
+
+var oauth_token = localStorage.getItem("oauth_token");
+var oauth_token_secret = localStorage.getItem("oauth_token_secret");
+
+var trending_vue = new Vue({
+        el: '#trending', 
+        data: {
+          trends: []
+        }
+    });
+
+
+
+
+function readyUp() {
+    if (oauth_token && oauth_token_secret) {
+          cb.setToken(oauth_token, oauth_token_secret);
+          console.log('cached');
+    } else {
+        // gets a request token
+        cb.__call(
+            "oauth_requestToken",
+            {oauth_callback: "oob"},
+            function (reply,rate,err) {
+                if (err) {
+                    console.log("error response or timeout exceeded" + err.error);
+                }
+                if (reply) {
+                    console.log(reply);
+                    
+                    // stores it in local memory
+                    cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+                    localStorage.setItem("oauth_token", reply.oauth_token);
+                    localStorage.setItem("oauth_token_secret", reply.oauth_token_secret);
+
+                    // gets the authorize screen URL
+                    // cb.__call(
+                    //     "oauth_authorize",
+                    //     {},
+                    //     function (auth_url) {
+                    //         console.log(auth_url)
+                    //         window.codebird_auth = window.open(auth_url);
+                    //     }
+                    // );
+
+                }
+            }
+        );
+    }
+    
+}
+
+function getTrends() {
+    params = {id: 1}
+    cb.__call(
+        "trends_place",
+        params,
+        function (response) {
+
+            var trending_topics = [];
+            for (var i = 0; i < response[0].trends.length; i++) {
+                var trend = response[0].trends[i];
+                trending_topics.push(trend);
+            }
+            trending_vue.trends = trending_topics;
+        }, true);
+}
+
+readyUp();
+getTrends();
