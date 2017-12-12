21812e59f9d98702eb3e57610118ba6eeadbb11f
pr <peter@peter.net>
2017-12-11 22:29:14 -0500

    done

--- js/searchhomepage.js
+++ js/searchhomepage.js
@@ -77,7 +77,7 @@ function getTrends() {
         function (response) {
 
             var trending_topics = [];
-            for (var i = 0; i < 5; i++) {
+            for (var i = 0; i < 8; i++) {
                 var trend = response[0].trends[i];
                 trending_topics.push(trend);
             }
