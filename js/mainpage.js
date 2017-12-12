8b9e4f413da3d4110bc87a7d590f4e836e6fb767
chenKuer <chenfjk@gmail.com>
2017-12-11 20:51:44 -0500

    left panel hide and show

--- js/mainpage.js
+++ js/mainpage.js
@@ -21,7 +21,9 @@ $(document).ready(function(){
 
  $(document).ready(function () {
                  $('#sidebarCollapse').on('click', function () {
+					 
                      $('#sidebar').toggleClass('active');
+					 
                  });
              });
 
